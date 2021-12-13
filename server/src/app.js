9a6a493724edaf86b401d37466de893df3d86c3a
Yusuf Kehinde Hussein <yusufkehinde11@gmail.com>
2021-12-13 15:41:55 +0100

    feat: added cors

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,10 @@ let express = require("express");
 let app = express();
 let router = require("./routes/index");
 let bodyParser = require("body-parser");
+let cors = require("cors");
 
+global.__root = __dirname + "/";
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
